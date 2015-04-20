1d29732daf2a23ccdc3d1de74a90f805d3f47fcc
julian <julianSelser@yahoo.com.ar>
2015-04-20 03:33:14 -0300

    Agrego rutas para: ver viajes de amigos, ver recomendados, aceptar/rechazar recomendacion y para recomendar un viaje a un amigo.

--- src/main/java/com/hax/controllers/TravelsController.java
+++ src/main/java/com/hax/controllers/TravelsController.java
@@ -1,6 +1,7 @@
 package com.hax.controllers;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -15,47 +16,131 @@ import org.json.JSONObject;
 @Path("flights")
 public class TravelsController {
     
-    private JSONObject flights;
+    private final JSONArray flights;
 
     public TravelsController() throws JSONException {
-        this.flights = new JSONObject().put("id", 1)
-                        .put("destination", "Acapulco")
-                        .put("airline", "Awesome Express")
-                        .put("flight-duration", 10)
-                        .put("flight-date", "5/5/2015")
-                        .put("flight-nro", 666)
-                        .put("price", 250.50);
+        this.flights = new JSONArray().put(
+                        new JSONObject().put("id", 1)
+                            .put("destination", "Acapulco")
+                            .put("origin", "Buenos Aires")
+                            .put("airline", "Awesome Express")
+                            .put("flight-duration", 10)
+                            .put("flight-date", "5/5/2015")
+                            .put("flight-num", 666)
+                            .put("price", 250.50)
+                        );
     }
     
-    @Path("list") 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String index(@QueryParam("from") String from, @QueryParam("to") String to, @QueryParam("from_date") String fromDate, @QueryParam("to_date") String toDate) throws JSONException
+    public String showFlights(@QueryParam("from") String from, @QueryParam("to") String to,
+            @QueryParam("from_date") String fromDate, @QueryParam("to_date") String toDate) throws JSONException
     {
         //ask despegar api for travels and filters them
-        return new JSONObject().put("flights", this.flights).toString();
+        return flights.toString();
     }
     
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public String saveTravel(String flight) throws JSONException
+    public String saveFlight(String flight) throws JSONException
     {
         /*
         *recive and save in the system one of the flight's json
         *to access the flight json just do:
         *
         *new JSONObject(flight)
+        *
         */
         return new JSONObject().put("success", true).toString();
     }
     
-    @Path("flights/{userId}")
+    @Path("/{userId}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String myTravels(@PathParam("userId") int userId) throws JSONException
+    public String myFlights(@PathParam("userId") int userId) throws JSONException
     {
-        //retrieves user flights
-        return new JSONObject().put("flights", this.flights).toString();
+        //retrieves {userId} flights
+        return flights.toString();
+    }
+    
+    @Path("/friends/{userId}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String friendsFlights(@PathParam("userId") int userId) throws JSONException
+    {
+        JSONObject aFriend = new JSONObject()
+                                .put("id", "1")
+                                .put("name", "Carlos")
+                                .put("apellido", "Ledesma")
+                                .put("flights", this.flights);
+                
+        //retrieves {userId} friends with their flights
+        return new JSONArray().put(aFriend).toString();
+    }
+    
+    @Path("friends/recommended/{userId}") 
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String showRecommended(@PathParam("userId") int userId) throws JSONException
+    {
+        //show flights recommended by friends to {userId}
+        return new JSONArray().put(new JSONObject()
+                                .put("id", 5)
+                                .put("friend-id", 6)
+                                .put("flight", this.flights.get(0))
+                            ).toString();
+    }
+    
+    @Path("friends/recommended/{userId}/{recommID}") 
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String acceptRecommendation(String postData, @PathParam("userId") int userId,
+            @PathParam("recommId") int recommId) throws JSONException
+    {
+        /*
+        *accepts recommendation from friend(in postData)
+        *use postData like:
+        *
+        *new JSONObject(postData)
+        *
+        */
+        return new JSONObject().put("success", true).toString();
+    }
+    
+    @Path("friends/recommended/{userId}/{recommID}") 
+    @DELETE
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String denyRecommendation(String postData, @PathParam("userId") int userId,
+            @PathParam("recommId") int recommId) throws JSONException
+    {
+        /*
+        *deny recommendation from friend(in postData)
+        *use postData like:
+        *
+        *new JSONObject(postData)
+        *
+        */
+        return new JSONObject().put("success", true).toString();
+    }
+    
+    @Path("/friends/recommend/{userId}/{friendId}")
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String recommendToFriend(String postData, @PathParam("userId") int userId,
+            @PathParam("friendId") int friendId) throws JSONException
+    {
+        /*
+        *{userId} recommends a flight to {friendId}
+        *additional data to perform recomendation in postData
+        *use postData like:
+        *
+        *new JSONObject(postData)
+        *
+        */
+        return new JSONObject().put("success", true).toString();
     }
 }
