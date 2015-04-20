13354b611f61317993c141f3c3a87519090bba17
julian <julianSelser@yahoo.com.ar>
2015-04-20 03:34:35 -0300

    Ahora se reciben datos del login en POST con JSON.

--- src/main/java/com/hax/controllers/LoginController.java
+++ src/main/java/com/hax/controllers/LoginController.java
@@ -1,10 +1,10 @@
 package com.hax.controllers;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -12,18 +12,26 @@ import org.json.JSONObject;
 @Path("login")
 public class LoginController
 {
-    
-   @GET
-   @Produces(MediaType.APPLICATION_JSON)
-   public String index()
-   {
-      return "form para ingresar al sistema";
-   }
-    
-   @POST
-   @Produces(MediaType.APPLICATION_JSON)
-   public String asd(@QueryParam("mail") String mail, @QueryParam("password") String password) throws JSONException
-   {
-      return new JSONObject().put("success", true).toString();
-   }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String index()
+    {
+        return "form para ingresar al sistema";
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String asd(String postData) throws JSONException
+    {
+        /*
+        *recieves postData to log user in
+        *use postData like:
+        *
+        *new JSONObject(postData)
+        *
+        */
+        return new JSONObject().put("success", true).toString();
+    }
 }
