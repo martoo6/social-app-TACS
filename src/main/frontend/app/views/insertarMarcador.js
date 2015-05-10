2f276862a421ecf5b7afdfe60229321984f65d37
Luciano Ingratta <lucho0988@hotmail.com>
2015-05-10 17:54:47 -0300

    agrego funciones google maps

---
+++ src/main/frontend/app/views/insertarMarcador.js
@@ -0,0 +1,20 @@
+function obtenerLatLng(resultados, status)
+{
+	if(status == google.maps.GeocoderStatus.OK)
+	{
+		//AJUSTA EL ZOOM DEL MAPA DE ACUERDO AL RESULTADO
+		mapa.fitBounds(resultados[0].geometry.viewport);
+		
+		//AGREGA UN MARCADOR
+		var marcador = new google.maps.Marker({ map: mapa,
+												position: resultados[0].geometry.location });
+	}
+	
+	else
+	{
+		alert("Geocoding no tuvo éxito debido a: " + status);
+	}
+
+};
+	
+decodificadorGeografico.geocode({'address': direccionLiteral}, obtenerLatLng);
---
+++ src/main/frontend/app/views/insertarRecorrido.js
@@ -0,0 +1,40 @@
+<div class="jumbotron" style="height:100%">
+    
+  <div id="mapa"></div>
+  <script>
+      
+    //CREA EL MAPA CON LA CONFIGURACION INICIAL
+    function inicializarMapa()
+    {
+        var vueloLatLng = [];
+        
+        var configuracionMapa = {   zoom: 5,
+                                    center: new google.maps.LatLng(-34.6037232, -58.3815931)
+                                };
+
+        //CREA UN NUEVO MAPA
+        var mapa = new google.maps.Map(document.getElementById('mapa'), configuracionMapa);
+        
+        vueloLatLng.push(new google.maps.LatLng(-34.6037232, -58.38159310000003));
+        vueloLatLng.push(new google.maps.LatLng(40.4167754, -3.7037901999999576));
+        vueloLatLng.push(new google.maps.LatLng(41.9027835, 12.496365500000024));
+        
+        var trazaVuelo = new google.maps.Polyline({ path: vueloLatLng,
+                                                    geodesic: true,
+                                                    strokeColor: '#FF0000',
+                                                    strokeOpacity: 1.0,
+                                                    strokeWeight: 2,
+                                                    clickable: false
+                                                });
+        trazaVuelo.setMap(mapa);
+        
+        //AJUSTA EL ZOOM PARA QUE SE VEA EL RECORRIDO
+        var bounds = new google.maps.LatLngBounds();
+        trazaVuelo.getPath().forEach(function(bound) {bounds.extend(bound);})
+        mapa.fitBounds(bounds);
+    };
+    
+    //MUESTRA EL MAPA AL CARGAR LA PAGINA
+    inicializarMapa();
+  </script>
+</div>
