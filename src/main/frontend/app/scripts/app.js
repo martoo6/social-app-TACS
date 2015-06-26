29ced2ffce6a2a121e394720b575820107848dfc
julian <julianSelser@yahoo.com.ar>
2015-06-25 22:09:21 -0300

    Saco agregados que estaban en el front para probarlo

--- src/main/frontend/app/scripts/app.js
+++ src/main/frontend/app/scripts/app.js
@@ -73,15 +73,10 @@ angular
     $rootScope.$on('fbLoginSuccess', function(name, response) {
       facebookUser.then(function(user) {
         user.api('/me').then(function(response) {
-          console.log(response);
-          window.fbResponse = response;
           $rootScope.loggedInUser = response;
         });
       });
-      window.fbUser = response;
-      window.fbStatus = response;
       $rootScope.fbStatus = response;
-      console.log(response);
       $http.defaults.headers.common['token'] = $rootScope.fbStatus.authResponse.accessToken;
       $.ajax({
             type: 'POST',
